fe15992925ed2b3b6e0ec274371f976243784894
laurie80 <laurie_dodre@outlook.fr>
2021-01-05 15:55:02 +0100

    fichier index calendrier

--- partie-9-dates/tp/index.php
+++ partie-9-dates/tp/index.php
@@ -31,7 +31,7 @@ $date = date('d/m/Y');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/img/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>TP partie 9 Les Formulaires</title>
+    <title>TP partie 9 Calendrier</title>
 </head>
 
 
