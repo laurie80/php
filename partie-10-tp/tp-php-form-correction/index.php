e0f016c7cd0bec37014ae8fd288ff9a8428fcc88
laurie80 <laurie_dodre@outlook.fr>
2021-01-11 13:37:10 +0100

    nouvelle correction sans bug

--- partie-10-tp/tp-php-form-correction/index.php
+++ partie-10-tp/tp-php-form-correction/index.php
@@ -32,7 +32,7 @@ require_once 'index_controller.php';
           <label for="lastName">Nom</label>
         </div>
         <div>
-          <input type="text" id="lastName" name="lastName" value="<?= isset($_POST['lastName']) ? $_POST['lastName'] : '' ?>" placeholder="ex : Doe">
+          <input type="text" id="lastName" name="lastName" value="<?= isset($_POST['lastName']) ? $_POST['lastName'] : '' ?>" placeholder="Mounivongs">
         </div>
         <p class="displayMessage">
           <?= isset($messageError['lastName']) ? $messageError['lastName'] : '' ?><?= isset($messageSuccess['lastName']) ? $messageSuccess['lastName'] : '' ?>
@@ -41,80 +41,80 @@ require_once 'index_controller.php';
           <label for="firstName">Prénom</label>
         </div>
         <div>
-          <input type="text" id="firstName" name="firstName" value="<?= (isset($_POST['firstName'])) ? $_POST['firstName'] : '' ?>" placeholder="ex : John">
+          <input type="text" id="firstName" name="firstName" value="<?= (isset($_POST['firstName'])) ? $_POST['firstName'] : '' ?>" placeholder="Anousone">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['firstName'])) ? $messageError['firstName'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['firstName']) ? $messageError['firstName'] : '' ?><?= isset($messageSuccess['firstName']) ? $messageSuccess['firstName'] : '' ?>
+        </p>
         <div>
           <label for="birthDate">Date de naissance</label>
         </div>
         <div>
           <input type="date" id="birthDate" name="birthDate" value="<?= (isset($_POST['birthDate'])) ? $_POST['birthDate'] : '' ?>">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['birthDate'])) ? $messageError['birthDate'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['birthDate']) ? $messageError['birthDate'] : '' ?><?= isset($messageSuccess['birthDate']) ? $messageSuccess['birthDate'] : '' ?>
+        </p>
         <div>
           <label for="birthCountry">Pays de naissance</label>
         </div>
         <div>
           <input type="text" id="birthCountry" name="birthCountry" value="<?= (isset($_POST['birthCountry'])) ? $_POST['birthCountry'] : '' ?>" placeholder="France">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['birthCountry'])) ? $messageError['birthCountry'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['birthCountry']) ? $messageError['birthCountry'] : '' ?><?= isset($messageSuccess['birthCountry']) ? $messageSuccess['birthCountry'] : '' ?>
+        </p>
         <div>
           <label for="nationality">Nationalité</label>
         </div>
         <div>
           <input type="text" id="nationality" name="nationality" value="<?= (isset($_POST['nationality'])) ? $_POST['nationality'] : '' ?>" placeholder="Française">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['nationality'])) ? $messageError['nationality'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['nationality']) ? $messageError['nationality'] : '' ?><?= isset($messageSuccess['nationality']) ? $messageSuccess['nationality'] : '' ?>
+        </p>
         <div>
           <label for="adress">Adresse</label>
         </div>
         <div>
           <input type="text" id="adress" name="adress" value="<?= (isset($_POST['adress'])) ? $_POST['adress'] : '' ?>" placeholder="01 rue blablabla">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['adress'])) ? $messageError['adress'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['adress']) ? $messageError['adress'] : '' ?><?= isset($messageSuccess['adress']) ? $messageSuccess['adress'] : '' ?>
+        </p>
         <div>
           <label for="mail">Adresse mail</label>
         </div>
         <div>
           <input type="email" id="mail" name="mail" value="<?= (isset($_POST['mail'])) ? $_POST['mail'] : '' ?>" placeholder="exemple@gmail.com">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['mail'])) ? $messageError['mail'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['mail']) ? $messageError['mail'] : '' ?><?= isset($messageSuccess['mail']) ? $messageSuccess['mail'] : '' ?>
+        </p>
         <div>
           <label for="phoneNumber">Numéro de téléphone</label>
         </div>
         <div>
-          <input type="text" id="phoneNumber" name="phoneNumber" value="<?= (isset($_POST['phoneNumber'])) ? $_POST['phoneNumber'] : '' ?>" placeholder="0123456789">
+          <input type="tel" id="phoneNumber" name="phoneNumber" value="<?= (isset($_POST['phoneNumber'])) ? $_POST['phoneNumber'] : '' ?>" placeholder="06.01.02.03.04">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['phoneNumber'])) ? $messageError['phoneNumber'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['phoneNumber']) ? $messageError['phoneNumber'] : '' ?><?= isset($messageSuccess['phoneNumber']) ? $messageSuccess['phoneNumber'] : '' ?>
+        </p>
         <div>
           <label for="degree">Diplôme</label>
         </div>
         <div>
           <select name="degree" id="degree">
-            <option value="" >Choisir</option>
-            <option value="1" <?= isset($_POST['degree']) && $_POST['degree'] == 1 ? "selected" : "" ?>>Sans diplôme</option>
-            <option value="2" <?= isset($_POST['degree']) && $_POST['degree'] == 2 ? "selected" : "" ?>>Bac</option>
-            <option value="3" <?= isset($_POST['degree']) && $_POST['degree'] == 3 ? "selected" : "" ?>>Bac+2</option>
-            <option value="4" <?= isset($_POST['degree']) && $_POST['degree'] == 4 ? "selected" : "" ?>>Bac+3 ou supérieur</option>
+            <option>Choisissez une option</option>
+            <option value="none" <?= isset($_POST['degree']) && $_POST['degree'] == 'none' ? 'selected' : '' ?>>Sans diplôme</option>
+            <option value="bac" <?= isset($_POST['degree']) && $_POST['degree'] == 'bac' ? 'selected' : '' ?>>Bac</option>
+            <option value="bac2" <?= isset($_POST['degree']) && $_POST['degree'] == 'bac2' ? 'selected' : '' ?>>Bac+2</option>
+            <option value="bac3supp" <?= isset($_POST['degree']) && $_POST['degree'] == 'bac3supp' ? 'selected' : '' ?>>Bac+3 ou supérieur</option>
           </select>
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['degree'])) ? $messageError['degree'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['degree']) ? $messageError['degree'] : '' ?><?= isset($messageSuccess['degree']) ? $messageSuccess['degree'] : '' ?>
+        </p>
 
         <div>
           <label for="poleEmploi">Numéro pôle emploi</label>
@@ -122,45 +122,45 @@ require_once 'index_controller.php';
         <div>
           <input type="text" id="poleEmploi" name="poleEmploi" value="<?= (isset($_POST['poleEmploi'])) ? $_POST['poleEmploi'] : '' ?>" placeholder="1234567A">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['poleEmploi'])) ? $messageError['poleEmploi'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['poleEmploi']) ? $messageError['poleEmploi'] : '' ?><?= isset($messageSuccess['poleEmploi']) ? $messageSuccess['poleEmploi'] : '' ?>
+        </p>
         <div>
           <label for="badgeNumber">Nombre de badge</label>
         </div>
         <div>
-          <input type="number" id="badgeNumber" name="badgeNumber" value="<?= (isset($_POST['badgeNumber'])) ? $_POST['badgeNumber'] : '' ?>" placeholder="00">
+          <input type="number" id="badgeNumber" name="badgeNumber" value="<?= (isset($_POST['badgeNumber'])) ? $_POST['badgeNumber'] : '' ?>" placeholder="10">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['badgeNumber'])) ? $messageError['badgeNumber'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['badgeNumber']) ? $messageError['badgeNumber'] : '' ?><?= isset($messageSuccess['badgeNumber']) ? $messageSuccess['badgeNumber'] : '' ?>
+        </p>
         <div>
           <label for="codeAcademy">Liens codeacademy</label>
         </div>
         <div>
           <input type="text" id="codeAcademy" name="codeAcademy" value="<?= (isset($_POST['codeAcademy'])) ? $_POST['codeAcademy'] : '' ?>" placeholder="https://votre-lien.fr">
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['codeAcademy'])) ? $messageError['codeAcademy'] : '' ?>
+        <p class="displayMessage">
+          <?= isset($messageError['codeAcademy']) ? $messageError['codeAcademy'] : '' ?><?= isset($messageSuccess['codeAcademy']) ? $messageSuccess['codeAcademy'] : '' ?>
+        </p>
+        <div>
+          <label for="heroStory">Si vous étiez un super héros/une super <br>héroïne, qui seriez-vous et pourquoi ?</label>
         </div>
         <div>
-          <label for="superHero">Si vous étiez un super héros/une super <br>héroïne, qui seriez-vous et pourquoi ?</label>
+          <textarea type="text" id="heroStory" name="heroStory"><?= (isset($_POST['heroStory'])) ? $_POST['heroStory'] : '' ?></textarea>
         </div>
-        <div>
-          <textarea type="text" id="superHero" name="superHero"><?= (isset($_POST['superHero'])) ? $_POST['superHero'] : '' ?></textarea>
-        </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['superHero'])) ? $messageError['superHero'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['heroStory']) ? $messageError['heroStory'] : '' ?><?= isset($messageSuccess['heroStory']) ? $messageSuccess['heroStory'] : '' ?>
+        </p>
         <div>
           <label for="hackStory">Racontez-nous un de vos "hacks" (pas <br>forcément technique ou informatique)</label>
         </div>
         <div>
           <textarea type="text" id="hackStory" name="hackStory"><?= (isset($_POST['hackStory'])) ? $_POST['hackStory'] : '' ?></textarea>
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['hackStory'])) ? $messageError['hackStory'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['hackStory']) ? $messageError['hackStory'] : '' ?><?= isset($messageSuccess['hackStory']) ? $messageSuccess['hackStory'] : '' ?>
+        </p>
         <div>
           <label for="experience">Avez vous déjà eu une expérience avec la <br>programmation et/ou l'informatique avant <br>de remplir ce formulaire ?</label>
         </div>
@@ -171,9 +171,9 @@ require_once 'index_controller.php';
           <input class="optionRadio" type="radio" id="experience" name="experience" value="Non" <?= isset($_POST['experience']) && $_POST['experience'] == 'Non' ? 'checked' : '' ?>></input>
           <label class="alignRadio" for="experience">Non</label>
         </div>
-        <div class="displayMessage">
-          <?= (isset($messageError['experience'])) ? $messageError['experience'] : '' ?>
-        </div>
+        <p class="displayMessage">
+          <?= isset($messageError['experience']) ? $messageError['experience'] : '' ?><?= isset($messageSuccess['experience']) ? $messageSuccess['experience'] : '' ?>
+        </p>
         <input class="btnColor mb-5" name="submit" type="submit" value="Envoyer">
       </form>
     </div>
--- partie-10-tp/tp-php-form-correction/index_controller.php
+++ partie-10-tp/tp-php-form-correction/index_controller.php
@@ -1,25 +1,21 @@
-  
 <?php
 
 $messageError = [];
 $messageSuccess = [];
 
 $regexName = '/^\D{2,19}$/';
-$regexDate = '/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/';
+$dateValid = '/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/';
 $regexPoleEmploi = '/^([0-9]{7}+[A-Z])$/';
-$regexAdress = '/^[0-9a-z ,.\'-]+$/i';
-//prend 0 ou +33 ou 0033 puis un chiffre de 1à9 ensuite 8 chiffres
-$regexPhoneNumber = '/^(0|\\+33|0033)[1-9][0-9]{8}$/';
-$regexNumber = '/^[0-9]+$/';
+$adressValid = '/^[0-9a-z ,.\'-]+$/i';
+$regexPhone = '/^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/';
+$regexBadges = '/^[0-9]{0,2}$/';
 
 $degreesArray = [
-    1 => 'Sans diplôme',
-    2 => 'Bac',
-    3 => 'Bac+2',
-    4 => 'Bac+3 ou supérieur'
+    1 => 'none',
+    2 => 'bac',
+    3 => 'bac2',
+    4 => 'bac3supp'
 ];
-
-var_dump($_POST);
 
 if (isset($_POST['submit'])) {
 
@@ -28,10 +24,9 @@ if (isset($_POST['submit'])) {
             $messageError['lastName'] = 'Le champs est vide';
         } else if (!preg_match($regexName, $_POST['lastName'])) {
             $messageError['lastName'] = 'Le nom n\'est pas valide';
-        } else if (strlen($_POST['lastName']) < 2 || strlen($_POST['lastName']) > 20) {
-            $messageError['lastName'] = 'Le nom doit contenir 2 à 20 caractères';
+        } else if (strlen($_POST['lastName']) < 2 || strlen($_POST['lastName']) > 50) {
+            $messageError['lastName'] = 'Le nom doit contenir 2 à 50 caractères';
         } else {
-            $secureLastName = htmlspecialchars($_POST['lastName']);
             $messageSuccess['lastName'] = '<i class="fas fa-check formValid"></i>';
         }
     }
@@ -41,28 +36,20 @@ if (isset($_POST['submit'])) {
             $messageError['firstName'] = 'Le champs est vide';
         } else if (!preg_match($regexName, $_POST['firstName'])) {
             $messageError['firstName'] = 'Le nom n\'est pas valide';
-        } else if (strlen($_POST['firstName']) < 2 || strlen($_POST['firstName']) > 20) {
-            $messageError['firstName'] = 'Le nom doit contenir de 2 à 20 caractères';
+        } else if (strlen($_POST['firstName']) < 2 || strlen($_POST['firstName']) > 50) {
+            $messageError['firstName'] = 'Le nom doit contenir de 2 à 50 caractères';
         } else {
-            $secureFirstName = htmlspecialchars($_POST['firstName']);
-            $messageError['firstName'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['firstName'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['birthDate'])) {
-
-        // Le format renvoyé par défaut est yyyy-mm-dd
-        // Je convertis le $_POST birthDate en timestamp pour ensuite pouvoir lui attribuer le bon format : dd/mm/yyyy
-        $birthDate = strtotime($_POST['birthDate']);
-        $dateCorrectFormat = date("d/m/Y", $birthDate);
-
         if (empty($_POST['birthDate'])) {
             $messageError['birthDate'] = 'Le champs est vide';
-        } else if (!preg_match($regexDate, $dateCorrectFormat)) {
+        } else if (!preg_match($dateValid, $_POST['birthDate'])) {
             $messageError['birthDate'] = 'Le format de date n\'est pas valide';
         } else {
-            $secureBirthDate = htmlspecialchars($dateCorrectFormat);
-            $messageError['birthDate'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['birthDate'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
@@ -71,11 +58,10 @@ if (isset($_POST['submit'])) {
             $messageError['birthCountry'] = 'Le champs est vide';
         } else if (!preg_match($regexName, $_POST['birthCountry'])) {
             $messageError['birthCountry'] = 'Le pays de naissance n\'est pas valide';
-        } else if (strlen($_POST['birthCountry']) < 2 || strlen($_POST['birthCountry']) > 40) {
-            $messageError['birthCountry'] = 'Le pays doit contenir de 2 à 40 caractères';
+        } else if (strlen($_POST['birthCountry']) < 2 || strlen($_POST['birthCountry']) > 50) {
+            $messageError['birthCountry'] = 'Le pays doit contenir de 2 à 50 caractères';
         } else {
-            $secureBirthCountry = htmlspecialchars($_POST['birthCountry']);
-            $messageError['birthCountry'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['birthCountry'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
@@ -84,118 +70,113 @@ if (isset($_POST['submit'])) {
             $messageError['nationality'] = 'Le champs est vide';
         } else if (!preg_match($regexName, $_POST['nationality'])) {
             $messageError['nationality'] = 'Le pays de naissance n\'est pas valide';
-        } else if (strlen($_POST['nationality']) < 2 || strlen($_POST['nationality']) > 40) {
-            $messageError['nationality'] = 'Le pays doit contenir de 2 à 40 caractères';
+        } else if (strlen($_POST['nationality']) < 2 || strlen($_POST['nationality']) > 50) {
+            $messageError['nationality'] = 'Le pays doit contenir de 2 à 50 caractères';
         } else {
-            $secureNationality = htmlspecialchars($_POST['nationality']);
-            $messageError['nationality'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['nationality'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['adress'])) {
         if (empty($_POST['adress'])) {
             $messageError['adress'] = 'Le champs est vide';
-        } else if (!preg_match($regexAdress, $_POST['adress'])) {
+        } else if (!preg_match($adressValid, $_POST['adress'])) {
             $messageError['adress'] = 'L\'adresse n\'est pas valide';
         } else if (strlen($_POST['adress']) < 2 || strlen($_POST['adress']) > 50) {
             $messageError['adress'] = 'L\'adresse doit contenir de 2 à 50 caractères';
         } else {
-            $secureAdress = htmlspecialchars($_POST['adress']);
-            $messageError['adress'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['adress'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['mail'])) {
-        $email = $_POST['mail'];
         if (empty($_POST['mail'])) {
             $messageError['mail'] = 'Le champs est vide';
-        } else if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $secureMail = $_POST['mail'];
-            $messageError['mail'] = '<i class="fas fa-check formValid"></i>';
+        } else if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
+            $messageError['mail'] = 'L\'adresse mail n\'est pas valide';
         } else {
-            $messageError['mail'] = 'L\'email n\'est pas valide';
+            $messageSuccess['mail'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['phoneNumber'])) {
         if (empty($_POST['phoneNumber'])) {
             $messageError['phoneNumber'] = 'Le champs est vide';
-        } else if (!preg_match($regexPhoneNumber, $_POST['phoneNumber'])) {
-            $messageError['phoneNumber'] = 'Le pays de naissance n\'est pas valide';
+        } else if (!preg_match($regexPhone, $_POST['phoneNumber'])) {
+            $messageError['phoneNumber'] = 'Le numéro de téléphone n\'est pas valide';
         } else {
-            $securePhoneNumber = htmlspecialchars($_POST['phoneNumber']);
-            $messageError['phoneNumber'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['phoneNumber'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
-    if (!array_key_exists($_POST['degree'], $degreesArray)) {
-        $messageError['degree'] = 'Veuillez sélectionner un choix';
-    } else {
-        $messageError['degree'] = '<i class="fas fa-check formValid"></i>';
-        $secureDegree = $_POST['degree'];
+    if (isset($_POST['degree'])) {
+        if (empty($_POST['degree'])) {
+            $messageError['degree'] = 'Le champs est vide';
+        } else if (!in_array($_POST['degree'], $degreesArray)) {
+            $messageError['degree'] = 'Choisissez une option';
+        } else {
+            $messageSuccess['degree'] = '<i class="fas fa-check formValid"></i>';
+        }
     }
 
     if (isset($_POST['poleEmploi'])) {
         if (empty($_POST['poleEmploi'])) {
             $messageError['poleEmploi'] = 'Le champs est vide';
         } else if (!preg_match($regexPoleEmploi, $_POST['poleEmploi'])) {
-            $messageError['poleEmploi'] = 'Le numéro n\'est pas valide';
+            $messageError['poleEmploi'] = 'Identifiant pôle emploi invalide';
         } else {
-            $securePoleEmploi = htmlspecialchars($_POST['poleEmploi']);
-            $messageError['poleEmploi'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['poleEmploi'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['badgeNumber'])) {
         if (empty($_POST['badgeNumber'])) {
             $messageError['badgeNumber'] = 'Le champs est vide';
-        } else if (!preg_match($regexNumber, $_POST['badgeNumber'])) {
-            $messageError['badgeNumber'] = 'Le numéro n\'est pas valide';
+        } else if (!preg_match($regexBadges, $_POST['badgeNumber'])) {
+            $messageError['badgeNumber'] = 'Doit contenir des chiffres. (1 à 2 maximum)';
         } else {
-            $securebadgeNumber = htmlspecialchars($_POST['badgeNumber']);
-            $messageError['badgeNumber'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['badgeNumber'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['codeAcademy'])) {
-        $url = $_POST['codeAcademy'];
         if (empty($_POST['codeAcademy'])) {
             $messageError['codeAcademy'] = 'Le champs est vide';
-        } else if (filter_var($url, FILTER_VALIDATE_URL)) {
-            $secureCodeAcademy = $_POST['codeAcademy'];
-            $messageError['codeAcademy'] = '<i class="fas fa-check formValid"></i>';
+        } else if (!filter_var($_POST['codeAcademy'], FILTER_VALIDATE_URL)) {
+            $messageError['codeAcademy'] = 'La syntaxe du lien n\'est pas bonne';
         } else {
-            $messageError['codeAcademy'] = 'Le lien n\'est pas valide';
+            $messageSuccess['codeAcademy'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
-    if (isset($_POST['superHero'])) {
-        if (empty($_POST['superHero'])) {
-            $messageError['superHero'] = 'Le champs est vide';
+    if (isset($_POST['heroStory'])) {
+        if (empty($_POST['heroStory'])) {
+            $messageError['heroStory'] = 'Le champs est vide';
+        } else if (strlen($_POST['heroStory']) < 2 || strlen($_POST['heroStory']) > 300) {
+            $messageError['heroStory'] = 'Doit contenir entre 2 et 300 caractères';
         } else {
-            $secureSuperHero = htmlspecialchars($_POST['superHero']);
-            $messageError['superHero'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['heroStory'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
     if (isset($_POST['hackStory'])) {
         if (empty($_POST['hackStory'])) {
             $messageError['hackStory'] = 'Le champs est vide';
+        } else if (strlen($_POST['hackStory']) < 2 || strlen($_POST['hackStory']) > 300) {
+            $messageError['hackStory'] = 'Doit contenir entre 2 et 300 caractères';
         } else {
-            $secureHackStory = htmlspecialchars($_POST['hackStory']);
-            $messageError['hackStory'] = '<i class="fas fa-check formValid"></i>';
+            $messageSuccess['hackStory'] = '<i class="fas fa-check formValid"></i>';
         }
     }
 
-    // Radio
+    // Si l'index 'experience' n'existe pas dans le $_POST, c'est que l'user n'a pas coché le radio, donc on retourne le message d'erreur
     if (!array_key_exists('experience', $_POST)) {
         $messageError['experience'] = 'Veuillez faire un choix';
     } else {
-        $secureExperience = $_POST['experience'];
+        $messageSuccess['experience'] = '<i class="fas fa-check formValid"></i>';
     }
 
     if (count($messageError) == 0) {
-
         $secureLastName = htmlspecialchars($_POST['lastName']);
         $secureFirstName = htmlspecialchars($_POST['firstName']);
         $secureBirthDate = htmlspecialchars($_POST['birthDate']);
@@ -206,9 +187,8 @@ if (isset($_POST['submit'])) {
         $securePhoneNumber = htmlspecialchars($_POST['phoneNumber']);
         $secureDegree = htmlspecialchars($_POST['degree']);
         $securePoleEmploi = htmlspecialchars($_POST['poleEmploi']);
-        $securebadgeNumber = htmlspecialchars($_POST['badgeNumber']);
-        $secureCodeAcademy = htmlspecialchars($_POST['codeAcademy']);
-        $secureSuperHero = htmlspecialchars($_POST['superHero']);
+        $secureBadgeNumber = htmlspecialchars($_POST['badgeNumber']);
+        $secureHeroStory = htmlspecialchars($_POST['heroStory']);
         $secureHackStory = htmlspecialchars($_POST['hackStory']);
         $secureExperience = htmlspecialchars($_POST['experience']);
     }
